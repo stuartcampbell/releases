a154f7f88e1cc9a36a84436c978f9feee4f45aeb
Stuart Campbell <scampbell@bnl.gov>
2020-12-31 22:03:51 -0500

    Changed to look at bluesky org

--- release.py
+++ release.py
@@ -21,7 +21,7 @@ class Repo:
     self.last_release_timestamp = self.releases[0].created_at.timestamp()
     self.commits = self.repo.get_commits(since=self.releases[0].created_at,sha='master')
 
-org = g.get_organization('elementary')
+org = g.get_organization('bluesky')
 
 repos = []
 for repo in org.get_repos():
